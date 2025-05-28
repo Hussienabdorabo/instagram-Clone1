64611391e9823b868bbab9708f56f522e88e7066
Hussienabdorabo <hussienali01030035741@gmail.com>
2025-05-28 19:42:24 +0300

    Update 00-laravel-deploy.sh

--- 00-laravel-deploy.sh
+++ 00-laravel-deploy.sh
@@ -6,5 +6,8 @@ echo "Caching config..."
 php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
+echo "Setting SQLite permissions..."
+chmod 775 /var/www/html/database/database.sqlite
+chown www-data:www-data /var/www/html/database/database.sqlite
 echo "Running migrations..."
 php artisan migrate --force
